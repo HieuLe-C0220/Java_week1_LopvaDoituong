3b0d1fcecf1faef9c0cd1a68d0d79d753326587b
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-14 00:57:49 +0700

    done

---
+++ LopFan/src/Fan.java
@@ -0,0 +1,30 @@
+public class Fan {
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+    public Fan() {
+    }
+    public Fan(int speed,boolean on,double radius,String color) {
+        this.speed = speed;
+        this.on = false;
+        this.radius = radius;
+        this.color = color;
+    }
+    public int getSpeed() {
+        return this.speed;
+    }
+    public String getOn() {
+        if (!this.on) {
+            return "This is Off";
+        } else {
+            return "This is On";
+        }
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public String getColor() {
+        return color;
+    }
+}
