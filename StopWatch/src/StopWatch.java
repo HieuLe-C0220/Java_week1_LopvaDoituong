6c32074e73dcd2eb0e64aa9bd62fcf34cfe735f0
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-14 10:49:33 +0700

    done

--- StopWatch/src/StopWatch.java
+++ StopWatch/src/StopWatch.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class StopWatch {
@@ -15,8 +17,23 @@ public class StopWatch {
 class testWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào độ dài của mảng:");
+        int size = scanner.nextInt();
+        long []array = new long[size];
+        Random rdom = new Random();
+        for (int i=0;i<array.length;i++) {
+            array[i] = rdom.nextInt(1000);
+        }
+        System.out.printf("The Array made a random with %4d element value random under 1000 \n",size);
+        System.out.println(Arrays.toString(array));
         StopWatch myWatch = new StopWatch();
-        
-        System.out.println(myWatch.getElapsedTime());
+        myWatch.start();
+        for (int i=0;i<size/2;i++) {
+            long temp = array[i];
+            array[i] = array[array.length-1-i];
+            array[array.length-1-i] = temp;
+        }
+        myWatch.stop();
+        System.out.println("Total time to process soft mechanic: "+myWatch.getElapsedTime()+" ms");
     }
 }
